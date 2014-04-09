b9eed961cf3505f1ecfe9a8bd592c8138061bfd4
USAMI Kosuke <usami.kosuke@gmail.com>
2014-04-10 06:51:07 +0900

    brew update & brew cask update

--- home/.setup/brew_setup.sh
+++ home/.setup/brew_setup.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-# update homebrew
-brew update
-brew upgrade
-
 # tap external repository
 brew tap phinze/cask
 
 # install package
+brew update
 brew install $(cat brew_leaves.txt)
 
 # cask install package
+brew cask update
 for cask in $(cat brew_casklist.txt) ; do
 	brew cask install $cask
 done
