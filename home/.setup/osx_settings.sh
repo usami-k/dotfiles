918104ce947858c03a2122e838c5062c6f621d51
USAMI Kosuke <usami.kosuke@gmail.com>
2014-09-29 16:04:11 +0900

    Disable App Nap

--- home/.setup/osx_settings.sh
+++ home/.setup/osx_settings.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+defaults write NSGlobalDomain NSAppSleepDisabled -bool yes
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 defaults write com.apple.finder QLEnableTextSelection -bool yes
