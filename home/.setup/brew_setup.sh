c5a99e7a2a833195c0c1af3a0da6fb6048c86eab
USAMI Kosuke <usami.kosuke@gmail.com>
2014-10-20 13:11:04 +0900

    add brew doctor / outdated / missing for system check

--- home/.setup/brew_setup.sh
+++ home/.setup/brew_setup.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
+
+# Prepare
 brew tap caskroom/cask
 brew tap peco/peco
+
+# Check
+brew doctor
 brew update
-brew upgrade
+brew outdated -v
+brew missing
+
+# Install
 brew install $(cat brew_leaves.txt)
 brew cask install $(cat brew_casklist.txt)
 brew cleanup
