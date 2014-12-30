f709272b7f2a99bade4fd19539737c84f34c954d
USAMI Kosuke <usami.kosuke@gmail.com>
2014-12-30 17:45:01 +0900

    atom package

---
+++ home/.setup/apm_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+apm install atom-runner
+apm install file-icons
+apm install jsonpp
+apm install language-csharp
+apm install language-haskell
+apm install language-swift
+apm install linter
+apm install linter-coffeelint
+apm install linter-jshint
+apm install linter-jsonlint
+apm install linter-ruby
+apm install open-recent
+apm install redpen
