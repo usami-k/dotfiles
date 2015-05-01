4e5f43bedd11e01617b99d78c37886f6e929b305
USAMI Kosuke <usami.kosuke@gmail.com>
2015-05-01 16:28:22 +0900

    Simplify brew_setup

--- home/.setup/brew_setup.sh
+++ home/.setup/brew_setup.sh
@@ -1,22 +1,8 @@
 #!/bin/sh
-
-# Prepare
 brew tap ioszip/ioszip
 brew tap thoughtbot/formulae
 brew tap caskroom/cask
 brew tap caskroom/fonts
-
-# Update
 brew update
-brew outdated -v
-brew doctor
-
-# Install
 brew install $(cat brew_leaves.txt)
-brew upgrade
-brew cleanup
-brew leaves > brew_leaves.txt
-brew cask install $(cat brew_casklist.txt)
-brew cask cleanup
-brew cask list > brew_casklist.txt
-
+brew cask install $(cat brew_cask_list.txt)
