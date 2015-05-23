65b503f4a46584da728696d3190e4857be4c7a22
USAMI Kosuke <usami.kosuke@gmail.com>
2015-05-23 10:49:39 +0900

    Setup DNX and ASP.NET

--- home/.setup/brew_setup.sh
+++ home/.setup/brew_setup.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-brew tap ioszip/ioszip
-brew tap thoughtbot/formulae
 brew tap caskroom/cask
 brew tap caskroom/fonts
+brew tap ioszip/ioszip
+brew tap thoughtbot/formulae
+brew tap aspnet/dnx
+
 brew update
 brew install $(cat brew_leaves.txt)
 brew cask install $(cat brew_cask_list.txt)
--- home/.setup/npm_setup.sh
+++ home/.setup/npm_setup.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+npm install -g bower
+npm install -g yo
+npm install -g grunt-cli
+npm install -g generator-aspnet
+
 npm install -g coffee-script
 npm install -g coffeelint
 npm install -g jshint
