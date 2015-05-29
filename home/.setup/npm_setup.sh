a750a8c23bb73e6cafd3c52cdefc7f4dd16aa41b
USAMI Kosuke <usami.kosuke@gmail.com>
2015-05-29 09:52:38 +0900

    Add comments

--- home/.setup/npm_setup.sh
+++ home/.setup/npm_setup.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
+
+# Yeoman
 npm install -g bower
 npm install -g yo
 npm install -g grunt-cli
 npm install -g generator-aspnet
 
+# CoffeeScript
 npm install -g coffee-script
+
+# linter
 npm install -g coffeelint
 npm install -g jshint
+
+# Test framework
 npm install -g mocha
+
