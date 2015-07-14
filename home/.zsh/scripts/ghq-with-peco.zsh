fc5415569d3bd2f67bb966735008370a6446112d
USAMI Kosuke <usami.kosuke@gmail.com>
2015-07-14 14:10:58 +0900

    ghq + peco

---
+++ home/.zsh/scripts/ghq-with-peco.zsh
@@ -0,0 +1,10 @@
+function peco-ghq () {
+    local selected_dir=$(ghq list -p | peco --query "$LBUFFER")
+    if [ -n "$selected_dir" ]; then
+        BUFFER="cd ${selected_dir}"
+        zle accept-line
+    fi
+    zle clear-screen
+}
+zle -N peco-ghq
+bindkey '^]' peco-ghq
