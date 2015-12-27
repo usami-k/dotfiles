032abad950a293c62405989445e8991d2fca90aa
USAMI Kosuke <usami.kosuke@gmail.com>
2015-12-27 13:56:40 +0900

    Clean up command notation

--- home/.setup/osx_settings.sh
+++ home/.setup/osx_settings.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Disable .DS_Store on network drive
-defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool YES
 
 # Enable text selection on QuickLook
-defaults write com.apple.finder QLEnableTextSelection -bool yes
+defaults write com.apple.finder QLEnableTextSelection -bool YES
 
 # Disable Photos.app HotPlug
 defaults write com.apple.ImageCapture disableHotPlug -bool YES
