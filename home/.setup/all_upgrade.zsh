4da13633654011fbad15072217730342fa3f0d36
USAMI Kosuke <usami.kosuke@gmail.com>
2016-01-29 20:37:54 +0900

    tidy up

--- home/.setup/all_upgrade.zsh
+++ home/.setup/all_upgrade.zsh
@@ -10,4 +10,4 @@ echo '## gem (bundler)'
 bundle update && bundle clean --force
 
 echo '## pip'
-pip3 install --upgrade $(pip3 list --outdated | cut -d' ' -f 1)
+pip3 install --upgrade $(pip3 list --outdated | cut -d' ' -f1)
