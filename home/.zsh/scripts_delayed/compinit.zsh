4af1d5e0bc62644d938a6da9366b8ea817b2b712
USAMI Kosuke <usami.kosuke@gmail.com>
2016-02-08 10:33:51 +0900

    Remove bash completion

--- home/.zsh/scripts_delayed/compinit.zsh
+++ home/.zsh/scripts_delayed/compinit.zsh
@@ -7,13 +7,6 @@ typeset -U fpath
 autoload compinit
 compinit -u -d "$HOME/.zsh_files/zcompdump"
 
-# bash 互換の補完関数
-autoload bashcompinit
-bashcompinit
-
-# stack の補完定義
-eval "$(stack --bash-completion-script stack)"
-
 # gibo の補完定義
 source $(brew --prefix)/share/zsh/site-functions/_gibo
 
