12f2b71ad96f98d5490b34bb9dffd4c54df94205
USAMI Kosuke <usami.kosuke@gmail.com>
2016-02-11 09:31:27 +0900

    Remove some commands

--- home/.zsh/geminfo.zsh
+++
@@ -1,9 +0,0 @@
-# http://oooooooo.hatenablog.com/entry/2013/06/15/162715
-function geminfo() {
-  if test $# -eq 1; then
-    gem spec $1 summary     | ruby -pe 'gsub(/(\-\-\-|\.\.\.\n)/, "")'
-    gem spec $1 description | ruby -pe 'gsub(/(\-\-\-|\.\.\.\n)/, "")'
-  else
-    bundle list | awk '/\* .+ / { print $2; system("gem spec "$2" summary;gem spec "$2" description") }' | ruby -pe 'gsub(/(\-\-\-|\.\.\.\n)/, "")'
-  fi
-}
--- home/.zsh/haskell-stack.zsh
+++
@@ -1,5 +0,0 @@
-if which stack >/dev/null ; then
-    alias ghc='stack ghc'
-    alias ghci='stack ghci'
-    alias runghc='stack runghc'
-fi
