3f610c4040ef746d052f56f71bb9d122e5be4f10
USAMI Kosuke <usami.kosuke@gmail.com>
2016-02-15 11:10:29 +0900

    Merge export settings

--- home/.zsh/editor.zsh
+++
@@ -1,2 +0,0 @@
-# EDITOR : CotEditorを別インスタンスで起動して終了を待つ
-export EDITOR="open -W -n -F -a CotEditor"
--- home/.zsh/export.zsh
+++ home/.zsh/export.zsh
@@ -1,3 +1,6 @@
+# EDITOR : CotEditorを別インスタンスで起動して終了を待つ
+export EDITOR="open -W -n -F -a CotEditor"
+
 # less : 1画面分のときは終了する、カラー表示する、行を折り返さない、画面をクリアしない
 export LESS=FRSX
 # less の履歴を保存しない
