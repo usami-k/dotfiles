dfd57acd26b4b71cc4b07cb0e91e752b808567d6
USAMI Kosuke <usami.kosuke@gmail.com>
2016-02-15 13:08:36 +0900

    Remove pushd setopt

--- home/.zsh/setopt.zsh
+++ home/.zsh/setopt.zsh
@@ -17,12 +17,6 @@ setopt ignore_eof
 
 # ディレクトリ名のみの入力でcdを行う
 setopt auto_cd
-# cdをpushdにする
-setopt auto_pushd
-# pushdのマイナスとプラスの意味を入れかえる（マイナスのときに新しいものから数える）
-setopt pushd_minus
-# pushdのディレクトリスタックで重複を避ける
-setopt pushd_ignore_dups
 
 # glob時に{}内で指定した文字を展開する
 setopt brace_ccl
