3f77ba89880a6ab92b73c9cd2362bd662c7a5c3c
USAMI Kosuke <usami.kosuke@gmail.com>
2016-02-25 12:39:25 +0900

    Disable vcs_info for svn / git-svn

--- home/.zsh/vcs-info.zsh
+++ home/.zsh/vcs-info.zsh
@@ -1,23 +1,24 @@
 # vcs_info
 autoload vcs_info
-# gitとsvnのみ有効にする
-zstyle ":vcs_info:*" enable git svn
+
+# gitのみ有効にする
+zstyle ":vcs_info:*" enable git
 # commitしていない変更をチェックする
-zstyle ":vcs_info:git*:*" check-for-changes true
+zstyle ":vcs_info:git:*" check-for-changes true
 # gitリポジトリに対して、変更情報とリポジトリ情報を表示する
-zstyle ":vcs_info:git*:*" formats "%u%c%m[%b]"
+zstyle ":vcs_info:git:*" formats "%u%c%m[%b]"
 # gitリポジトリに対して、コンフリクトなどの情報を表示する
-zstyle ":vcs_info:git*:*" actionformats "%u%c<%a>%m[%b]"
+zstyle ":vcs_info:git:*" actionformats "%u%c<%a>%m[%b]"
 # addしていない変更があることを示す文字列
-zstyle ":vcs_info:git*:*" unstagedstr "<U>"
+zstyle ":vcs_info:git:*" unstagedstr "<U>"
 # commitしていないstageがあることを示す文字列
-zstyle ":vcs_info:git*:*" stagedstr "<S>"
+zstyle ":vcs_info:git:*" stagedstr "<S>"
 # stgit使用時、パッチが当たっていればパッチ名を表示する
-zstyle ":vcs_info:git*:*" patch-format "{%p}"
+zstyle ":vcs_info:git:*" patch-format "{%p}"
 # stgit使用時、パッチが当たっていなければ何も表示しない
-zstyle ":vcs_info:git*:*" nopatch-format ""
+zstyle ":vcs_info:git:*" nopatch-format ""
 # hook関数を定義
-zstyle ":vcs_info:git*+set-message:*" hooks my_vcs_info
+zstyle ":vcs_info:git+set-message:*" hooks my_vcs_info
 
 # git：まだpushしていないcommitがあるかチェックする
 my_git_info_push () {
