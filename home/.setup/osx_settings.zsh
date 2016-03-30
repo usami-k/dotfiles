469342dc811129427cba665781393c0f7a37e6a9
USAMI Kosuke <usami.kosuke@gmail.com>
2016-03-30 10:30:40 +0900

    Flatten Services menu

--- home/.setup/osx_settings.zsh
+++ home/.setup/osx_settings.zsh
@@ -3,6 +3,9 @@
 # Disable .DS_Store on network drive
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool YES
 
+# Flatten Services menu
+defaults write NSGlobalDomain NSServicesMinimumItemCountForContextSubmenu -int 32
+
 # Enable text selection on QuickLook
 defaults write com.apple.finder QLEnableTextSelection -bool YES
 
