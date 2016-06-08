760d64ec9e0d7578fa1045a9a9cd062421e5a7f5
USAMI Kosuke <usami.kosuke@gmail.com>
2016-06-08 18:23:27 +0900

    Optimize performance : pip completion

--- home/.zsh/completion.zsh
+++ home/.zsh/completion.zsh
@@ -26,6 +26,16 @@ zstyle ':completion:*' matcher-list '' '+m:{a-z}={A-Z}' '+m:{A-Z}={a-z}'
 # 今いるディレクトリを補完候補から外す
 zstyle ':completion:*' ignore-parents parent pwd ..
 
-# pip completion
-eval "$(pyenv exec pip completion --zsh)"
+# eval "$(pyenv exec pip completion --zsh)"
+# pip zsh completion start
+function _pip_completion {
+  local words cword
+  read -Ac words
+  read -cn cword
+  reply=( $( COMP_WORDS="$words[*]" \
+             COMP_CWORD=$(( cword-1 )) \
+             PIP_AUTO_COMPLETE=1 $words[1] ) )
+}
+compctl -K _pip_completion pip
+# pip zsh completion end
 
