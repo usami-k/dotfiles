3aa613b0c413dd47dab0dc81fc74b3e5513cd50d
USAMI Kosuke <usami.kosuke@gmail.com>
2016-08-16 14:50:49 +0900

    tidy up : glob.zsh

--- home/.zsh/shell.zsh
+++ home/.zsh/shell.zsh
@@ -12,3 +12,14 @@ setopt hist_ignore_all_dups
 setopt hist_reduce_blanks
 # スペースで始まる入力はヒストリに追加しない
 setopt hist_ignore_space
+
+# glob時に{}内で指定した文字を展開する
+setopt brace_ccl
+# globで#、~、^を有効にする
+setopt extended_glob
+# glob時にドット始まりのファイルも対象にする
+setopt glob_dots
+# glob時にファイル名を数値順にソートする
+setopt numeric_glob_sort
+# イコールの後でもファイル名展開を行う
+setopt magic_equal_subst
