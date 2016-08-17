8f21c3266212eb1a43713230ca238e0a8ea95c45
USAMI Kosuke <usami.kosuke@gmail.com>
2016-08-17 09:43:42 +0900

    Remove pushd / popd

--- home/.setup/setup.zsh
+++ home/.setup/setup.zsh
@@ -1,22 +1,19 @@
 #!/bin/zsh
-pushd $(dirname $0)
 
 echo '## homebrew'
 brew update
-brew install $(cat brew_leaves | sed "/^#/d")
+brew install $(cat $(dirname $0)/brew_leaves | sed "/^#/d")
 swiftenv rehash
 
 echo '## homebrew cask'
 brew tap caskroom/cask
 brew tap caskroom/fonts
-brew cask install $(cat cask_list | sed "/^#/d")
+brew cask install $(cat $(dirname $0)/cask_list | sed "/^#/d")
 
 echo '## gem'
-gem install $(cat gem_list | sed "/^#/d")
+gem install $(cat $(dirname $0)/gem_list | sed "/^#/d")
 rbenv rehash
 
 echo '## pip'
-pip install $(cat pip_list | sed "/^#/d")
+pip install $(cat $(dirname $0)/pip_list | sed "/^#/d")
 pyenv rehash
-
-popd
