3d4253ff36fd1b4d0e7fccfe1cbc0dd7663fbec9
USAMI Kosuke <usami.kosuke@gmail.com>
2016-09-12 11:07:24 +0900

    Run rehash before gem / pip

--- home/.setup/upgrade.zsh
+++ home/.setup/upgrade.zsh
@@ -4,14 +4,14 @@ echo '## homebrew'
 brew update
 brew upgrade --all && brew cleanup
 swiftenv rehash
+rbenv rehash
+pyenv rehash
 
 echo '## homebrew cask'
 brew cask install $(brew cask list) && brew cask cleanup
 
 echo '## gem'
 yes | gem update && gem cleanup
-rbenv rehash
 
 echo '## pip'
 pip install --upgrade $(pip list --outdated | cut -d' ' -f1)
-pyenv rehash
