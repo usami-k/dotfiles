316b93cf5e7f61c30e7609f6a2512c4e22d4882b
USAMI Kosuke <usami.kosuke@gmail.com>
2016-10-19 16:56:30 +0900

    Add DEVELOPER_DIR for homebrew

--- home/.zsh/brew.zsh
+++ home/.zsh/brew.zsh
@@ -1,2 +1,2 @@
-# brew : 各種ツールの PATH を除いて実行する
-alias brew="PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin brew"
+# brew : 各種ツールの PATH を除いて実行する、Xcode を指定する
+alias brew="PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin DEVELOPER_DIR=/Applications/Xcode_8.0.app/Contents/Developer brew"
