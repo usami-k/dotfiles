05d7883bdf764d3a59eee113493caeb78e7e3cc4
USAMI Kosuke <usami.kosuke@gmail.com>
2016-11-27 15:43:25 +0900

    Replace alias to configuration file or varible

--- home/.zsh/shell_commands.zsh
+++ home/.zsh/shell_commands.zsh
@@ -11,12 +11,6 @@ alias mv="mv -v"
 # rm : 削除前に確認する、進捗を表示する
 which grm >/dev/null && alias rm="grm -I -v"
 
-# lv : カラー表示に対応する
-alias lv="lv -c"
-
-# grep : カラー表示する
-alias grep="grep --color=auto"
-
 # diff : 常にカラー表示する、Unified 形式にする
 alias diff="diff --unified"
 which colordiff >/dev/null && alias diff="colordiff --unified"
@@ -30,6 +24,9 @@ alias rename="noglob zmv -W"
 
 # EDITOR : CotEditor
 export EDITOR="cot --wait"
+
+# grep : カラー表示する
+export GREP_OPTIONS="--color=auto"
 
 # less : 1画面分のときは終了する、カラー表示する、行を折り返さない、画面をクリアしない
 export LESS=FRSX
