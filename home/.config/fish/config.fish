52581fad935a40d6d4809faeb19f9d1e0ded8c98
USAMI Kosuke <usami.kosuke@gmail.com>
2017-04-29 21:55:50 +0900

    Tidy up

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,3 +1,43 @@
+# ls : show all, colorize, sort by version number
+if command -sq gls
+    alias ls 'gls -A --color=auto --sort=version'
+end
+if command -sq gdircolors
+    set -x LS_COLORS (string match -r '^.(.*).$' (gdircolors -c | string split ' ')[3])[2]
+end
+
+# cp : show files
+alias cp 'cp -v'
+
+# mv : show files
+alias mv 'mv -v'
+
+# rm : prompt once before removing, show files
+if command -sq grm
+    alias rm 'grm -I -v'
+else
+    alias rm 'rm -i -v'
+end
+
+# diff : unified format, colorize
+if command -sq colordiff
+    alias diff 'colordiff --unified'
+else
+    alias diff 'diff --unified'
+end
+
+# grep : colorize
+set -x GREP_OPTIONS '--color=auto'
+
+# less : colorize, chop long lines, no clearing screen
+set -x LESS 'RSX'
+
+# jq : colorize
+alias jq 'jq -C'
+
+# javascript
+alias jsc '/System/Library/Frameworks/JavaScriptCore.framework/Resources/jsc'
+
 # fzf
 set -x FZF_LEGACY_KEYBINDINGS 0
 
@@ -6,46 +46,6 @@ if status is-login
     source $HOME/.homesick/repos/homeshick/homeshick.fish
     source $HOME/.homesick/repos/homeshick/completions/homeshick.fish
 end
-
-# ls : show all, colorize, sort by version number
-if command -sq gls
-    alias ls "gls -A --color=auto --sort=version"
-end
-if command -sq gdircolors
-    set -x LS_COLORS (string match -r '^.(.*).$' (gdircolors -c | string split ' ')[3])[2]
-end
-
-# cp : show files
-alias cp "cp -v"
-
-# mv : show files
-alias mv "mv -v"
-
-# rm : prompt once before removing, show files
-if command -sq grm
-    alias rm "grm -I -v"
-else
-    alias rm "rm -i -v"
-end
-
-# diff : unified format, colorize
-if command -sq colordiff
-    alias diff "colordiff --unified"
-else
-    alias diff "diff --unified"
-end
-
-# grep : colorize
-set -x GREP_OPTIONS "--color=auto"
-
-# less : colorize, chop long lines, no clearing screen
-set -x LESS "RSX"
-
-# jq : colorize
-alias jq "jq -C"
-
-# javascript
-alias jsc "/System/Library/Frameworks/JavaScriptCore.framework/Resources/jsc"
 
 # go
 set -x GOPATH $HOME/go
@@ -60,5 +60,5 @@ end
 
 # EDITOR : CotEditor
 if command -sq cot
-    set -x EDITOR "cot --wait"
+    set -x EDITOR 'cot --wait'
 end
