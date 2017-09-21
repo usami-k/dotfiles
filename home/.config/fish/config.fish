a84228635f2c00a03f98a942e2597440d3b40360
USAMI Kosuke <usami.kosuke@gmail.com>
2017-09-21 15:42:30 +0900

    Set fish git prompt char

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,3 +1,9 @@
+# fish git prompt
+set __fish_git_prompt_char_dirtystate '*'
+set __fish_git_prompt_char_stagedstate '+'
+set __fish_git_prompt_char_stashstate '$'
+set __fish_git_prompt_char_untrackedfiles '?'
+
 # fzf : exact match, custom layout
 set --export FZF_DEFAULT_OPTS '--exact --inline-info --reverse --height=10'
 
