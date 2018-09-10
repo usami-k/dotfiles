c311c350ea9a9583bdfe0a82a6e2432567e8433c
USAMI Kosuke <usami.kosuke@gmail.com>
2018-09-10 11:12:59 +0900

    No delete system files

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -14,12 +14,6 @@ else
     alias ls 'ls -A'
 end
 
-# Erase unused functions
-if command -sq greadlink
-    rm -f (greadlink -f (brew --prefix)/share/fish/functions/ll.fish)
-    rm -f (greadlink -f (brew --prefix)/share/fish/functions/la.fish)
-end
-
 # cp : show files
 alias cp 'cp -v'
 
