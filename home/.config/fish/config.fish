c33a3b06c1853e45b3a143387eee07b71b5022f8
USAMI Kosuke <usami.kosuke@gmail.com>
2018-10-06 13:56:23 +0900

    Enable direnv log

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -7,9 +7,6 @@ set __fish_git_prompt_char_untrackedfiles '?'
 # fzf : exact match, custom layout
 set --export FZF_DEFAULT_OPTS '--exact --inline-info --reverse --height=10'
 
-# direnv : quiet cd
-set --export DIRENV_LOG_FORMAT ''
-
 # ls : show all, colorize, sort by version number
 if command -sq gls
     alias ls 'gls -A --sort=version'
