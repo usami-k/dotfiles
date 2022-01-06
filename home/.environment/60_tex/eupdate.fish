ee744e7072ebabbc2d614bc667a539d92e9f0f59
USAMI Kosuke <usami.kosuke@gmail.com>
2022-01-06 09:24:12 +0900

    homwbrew can install latexindent

--- home/.environment/60_tex/eupdate.fish
+++ home/.environment/60_tex/eupdate.fish
@@ -2,7 +2,4 @@
 
 if command --query tlmgr
     tlmgr update --self --all --reinstall-forcibly-removed
-
-    # Install cpan modules for latexindent
-    yes | /usr/local/texlive/2021/texmf-dist/doc/support/latexindent/latexindent-module-installer.pl
 end
