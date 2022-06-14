497e4eba83c0eedcb8fdd6e3743a8e387891cb81
USAMI Kosuke <usami.kosuke@gmail.com>
2022-06-14 10:02:16 +0900

    Fix script

--- home/.environment/60_tex/eupdate.fish
+++ home/.environment/60_tex/eupdate.fish
@@ -7,7 +7,7 @@ end
 if command --query cjk-gs-integrate-macos
     if ! test -d /usr/local/texlive/texmf-local/fonts/opentype/Yu
         # 游フォントの設定
-        mkdir /usr/local/texlive/texmf-local/fonts/opentype/Yu
+        mkdir -p /usr/local/texlive/texmf-local/fonts/opentype/Yu
         pushd /usr/local/texlive/texmf-local/fonts/opentype/Yu
         ln -s /System/Library/AssetsV2/com_apple_MobileAsset_Font7/*/AssetData/YuGothic-Medium.otf .
         ln -s /System/Library/AssetsV2/com_apple_MobileAsset_Font7/*/AssetData/YuGothic-Bold.otf .
