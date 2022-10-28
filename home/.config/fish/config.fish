57d8e014c91d79a44c209c3391e5be95b79baf20
USAMI Kosuke <usami.kosuke@gmail.com>
2022-10-28 22:58:52 +0900

    Remove old settings

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -67,11 +67,6 @@ if command -sq direnv
     direnv hook fish | source
 end
 
-# pyenv
-if command -sq pyenv
-    pyenv init - | source
-end
-
 # rbenv
 if command -sq rbenv
     rbenv init - | source
@@ -80,11 +75,6 @@ end
 # asdf
 if test -f /opt/homebrew/opt/asdf/libexec/asdf.fish
     source /opt/homebrew/opt/asdf/libexec/asdf.fish
-end
-
-# go
-if command -sq go
-    fish_add_path -m (go env GOPATH)/bin
 end
 
 # rust
