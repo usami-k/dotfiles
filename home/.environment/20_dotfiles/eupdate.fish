298556398b11d5813e81c92ad44023be6adafed1
USAMI Kosuke <usami.kosuke@gmail.com>
2022-11-13 15:50:43 +0900

    backup dotfiles

--- home/.environment/20_dotfiles/eupdate.fish
+++ home/.environment/20_dotfiles/eupdate.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
 ~/.homesick/repos/homeshick/bin/homeshick --batch pull
-~/.homesick/repos/homeshick/bin/homeshick --batch link
+~/.homesick/repos/homeshick/bin/homeshick --batch --force link
 
 fisher update
