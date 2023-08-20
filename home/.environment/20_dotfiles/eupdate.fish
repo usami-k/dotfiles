52b572416fd0ba59667b993963a0c56d57d015c2
USAMI Kosuke <usami.kosuke@gmail.com>
2023-08-20 10:20:44 +0900

    Add envs script

--- home/.environment/20_dotfiles/eupdate.fish
+++ home/.environment/20_dotfiles/eupdate.fish
@@ -2,8 +2,3 @@
 
 ~/.homesick/repos/homeshick/bin/homeshick --batch pull
 ~/.homesick/repos/homeshick/bin/homeshick --batch --force link
-
-fisher update
-fish_update_completions
-
-asdf plugin-update --all
---
+++ home/.environment/50_envs/eupdate.fish
@@ -0,0 +1,6 @@
+#!/usr/bin/env fish
+
+fisher update
+fish_update_completions
+
+asdf plugin-update --all
