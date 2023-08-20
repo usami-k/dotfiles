2f310747fd0e5f5835c12168c865ee27d6b9191f
USAMI Kosuke <usami.kosuke@gmail.com>
2023-08-20 12:33:13 +0900

    git pull

--- home/.environment/40_ghq/eupdate.fish
+++ home/.environment/40_ghq/eupdate.fish
@@ -2,12 +2,24 @@
 
 if command --query ghq
     function env_installed
-        env GHQ_ROOT=$HOME/repos ghq list
+        ghq list
     end
 
     for repo in (env_installed)
         pushd $HOME/repos/$repo
         git config rerere.enabled true
+        popd
+    end
+end
+
+if command --query ghq
+    function env_personal
+        ghq list usami-k/
+    end
+
+    for repo in (env_personal)
+        pushd $HOME/repos/$repo
+        git pull --quiet
         popd
     end
 end
