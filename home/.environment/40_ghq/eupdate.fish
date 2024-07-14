a623cc22aed64e8149ba26d7efcabe184337e60b
USAMI Kosuke <usami.kosuke@gmail.com>
2024-07-14 14:17:03 +0900

    Remove local config rerere

--- home/.environment/40_ghq/eupdate.fish
+++ home/.environment/40_ghq/eupdate.fish
@@ -1,18 +1,6 @@
 #!/usr/bin/env fish
 
 if command --query ghq
-    function env_installed
-        ghq list
-    end
-
-    for repo in (env_installed)
-        pushd $HOME/repos/$repo
-        git config rerere.enabled true
-        popd
-    end
-end
-
-if command --query ghq
     function env_personal
         ghq list usami-k/
     end
