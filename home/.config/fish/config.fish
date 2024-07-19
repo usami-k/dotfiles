6e720ea71762bffd9748ea9e62a6f83a6a56b9a4
USAMI Kosuke <usami.kosuke@gmail.com>
2024-07-19 11:42:18 +0900

    Remove completion setting

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -50,7 +50,6 @@ end
 # homeshick
 if test -d $HOME/.homesick/repos/homeshick
     source $HOME/.homesick/repos/homeshick/homeshick.fish
-    source $HOME/.homesick/repos/homeshick/completions/homeshick.fish
 end
 
 # direnv
