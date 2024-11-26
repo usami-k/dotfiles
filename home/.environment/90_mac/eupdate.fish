0c5066a699971e4726ae4552020f59e04d520a1c
USAMI Kosuke <usami.kosuke@gmail.com>
2024-11-26 14:31:59 +0900

    style: ブール値の書式を修正

--- home/.environment/90_mac/eupdate.fish
+++ home/.environment/90_mac/eupdate.fish
@@ -4,23 +4,23 @@
 defaults write NSGlobalDomain NSServicesMinimumItemCountForContextSubmenu -int 32
 
 # Show hidden files
-defaults write com.apple.finder AppleShowAllFiles -bool true
+defaults write com.apple.finder AppleShowAllFiles -bool YES
 
 # Enable text selection on QuickLook
-defaults write com.apple.finder QLEnableTextSelection -bool true
+defaults write com.apple.finder QLEnableTextSelection -bool YES
 
 # Disable .DS_Store on network drive
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool YES
 
 # Disable shadow on screen capture
-defaults write com.apple.screencapture disable-shadow -bool true
+defaults write com.apple.screencapture disable-shadow -bool YES
 
 # Disable automatic launching Photos on connecting device
-defaults write com.apple.ImageCapture disableHotPlug -bool true
+defaults write com.apple.imagecapture disableHotPlug -bool YES
 
 # Disable automatic reopening of last project
 defaults write com.apple.dt.Xcode ApplePersistenceIgnoreState -bool YES
 
 # LaunchPad : Reset app positions
-defaults write com.apple.dock ResetLaunchPad -bool true
+defaults write com.apple.dock ResetLaunchPad -bool YES
 killall Dock
