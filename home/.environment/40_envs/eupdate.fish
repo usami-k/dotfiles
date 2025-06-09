b9bb407bea41d37cde9b63995be1bf56b1a091f0
USAMI Kosuke <usami.kosuke@gmail.com>
2025-06-09 14:06:08 +0900

    eupdateスクリプトにswiftly、ghcup、fnmを追加

--- home/.environment/40_envs/eupdate.fish
+++ home/.environment/40_envs/eupdate.fish
@@ -1,5 +1,15 @@
 #!/usr/bin/env fish
 
+swiftly update --assume-yes
+
 rustup update
+
+ghcup --no-verbose install ghc
+ghcup --no-verbose install cabal
+ghcup --no-verbose install hls
+ghcup --no-verbose install stack
+
+fnm install --lts
+fnm default system
 
 gh extension upgrade --all
