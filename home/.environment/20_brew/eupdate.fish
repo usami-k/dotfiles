df8c91e7cfc846770518186d450473b54e3ccdac
USAMI Kosuke <k_usami@yumemi.co.jp>
2025-07-13 15:57:03 +0900

    eupdateスクリプトを修正し、brewコマンドの存在確認後に`brew doctor`を実行するように変更。また、不要なコマンドを削除し、実行しない場合のメッセージを追加。

--- home/.environment/20_brew/eupdate.fish
+++ home/.environment/20_brew/eupdate.fish
@@ -2,12 +2,7 @@
 
 if type --query brew
     brew bundle --global
+    brew doctor
+else
+    echo 'Skip brew'
 end
-
-# Update .Brewfile in dotfiles
-if type --query brew
-    brew bundle --global dump --tap --brew --cask --mas --force
-    ~/.homesick/repos/homeshick/bin/homeshick --batch check
-end
-
-brew doctor
