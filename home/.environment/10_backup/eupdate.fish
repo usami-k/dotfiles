3f4e1cfc2af1596c972198e8f8d70c57108bf856
USAMI Kosuke <k_usami@yumemi.co.jp>
2025-07-23 23:31:20 +0900

    backup ghq list

--- home/.environment/10_backup/eupdate.fish
+++ home/.environment/10_backup/eupdate.fish
@@ -11,3 +11,10 @@ end
 
 echo '--- keychains backup'
 rsync --archive --delete-during ~/Library/Keychains ~/backup/
+
+if type --query ghq
+    echo '--- ghq backup'
+    ghq list > ~/backup/ghq_list.txt
+else
+    echo 'Skip ghq'
+end
