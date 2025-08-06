99c51e80c4fdaf8684540f1f833bdd60cbac5bfc
USAMI Kosuke <usami.kosuke@gmail.com>
2025-08-06 19:50:19 +0900

    eupdate.fish: rsyncのオプションを--delete-duringから--delete-delayに変更

--- home/.environment/10_backup/eupdate.fish
+++ home/.environment/10_backup/eupdate.fish
@@ -4,13 +4,13 @@ if type --query mackup
     echo '--- mackup backup'
     mackup --force backup
     mackup --force uninstall
-    rsync --archive --delete-during ~/.backup/Mackup ~/backup/
+    rsync --archive --delete-delay ~/.backup/Mackup ~/backup/
 else
     echo 'Skip mackup'
 end
 
 echo '--- keychains backup'
-rsync --archive --delete-during ~/Library/Keychains ~/backup/
+rsync --archive --delete-delay ~/Library/Keychains ~/backup/
 
 if type --query ghq
     echo '--- ghq backup'
@@ -21,7 +21,7 @@ end
 
 if test -d /opt/git
     echo '--- local git repos backup'
-    rsync --archive --delete-during /opt/git ~/backup/
+    rsync --archive --delete-delay /opt/git ~/backup/
 else
     echo 'Skip local git repos (directory not found)'
 end
