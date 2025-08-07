7b46679530b3b43f4c42f3107b44886eea8ab0aa
USAMI Kosuke <usami.kosuke@gmail.com>
2025-08-07 16:18:18 +0900

    フォーマット

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -1,27 +1,27 @@
-local wezterm = require 'wezterm'
+local wezterm = require "wezterm"
 
 function get_appearance()
   if wezterm.gui then
-      return wezterm.gui.get_appearance()
-    end
-  return 'Dark'
+    return wezterm.gui.get_appearance()
+  end
+  return "Light"
 end
 
 function scheme_for_appearance(appearance)
-  if appearance:find 'Dark' then
-    return 'OneHalfDark'
+  if appearance:find "Dark" then
+    return "OneHalfDark"
   else
-    return 'OneHalfLight'
+    return "OneHalfLight"
   end
 end
 
 local config = wezterm.config_builder()
 config.color_scheme = scheme_for_appearance(get_appearance())
-config.font = wezterm.font_with_fallback {'0xProto Nerd Font', 'IBM Plex Sans JP'}
+config.font = wezterm.font_with_fallback { "0xProto Nerd Font", "IBM Plex Sans JP" }
 config.font_size = 14
-config.default_cursor_style = 'BlinkingBlock'
-config.default_prog = {'/opt/homebrew/bin/fish'}
-config.skip_close_confirmation_for_processes_named = {'fish'}
+config.default_cursor_style = "BlinkingBlock"
+config.default_prog = { "/opt/homebrew/bin/fish" }
+config.skip_close_confirmation_for_processes_named = { "fish" }
 config.hide_tab_bar_if_only_one_tab = true
 config.initial_cols = 88
 config.initial_rows = 40
