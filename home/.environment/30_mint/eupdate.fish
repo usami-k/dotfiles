8b16f5f9e0fba8aada71523c986a1e0712487376
USAMI Kosuke <usami.kosuke@gmail.com>
2025-11-21 10:26:01 +0900

    Remove mint environment

--- home/.environment/30_mint/eupdate.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-
-set --global script_dir (realpath (dirname (status filename)))
-
-if type --query mint
-    mint bootstrap --mintfile $script_dir/Mintfile
-else
-    echo 'Skip mint'
-end
