2ffddefa983716bd3d855b29b4fdd91035f49eab
USAMI Kosuke <usami.kosuke@gmail.com>
2025-12-14 15:19:55 +0900

    eupdate.fish: cjk-gs-integrate の設定を削除

--- home/.environment/50_tex/eupdate.fish
+++ home/.environment/50_tex/eupdate.fish
@@ -5,19 +5,3 @@ if type --query tlmgr
 else
     echo 'Skip tlmgr'
 end
-
-if type --query cjk-gs-integrate
-    if ! test -d /usr/local/texlive/texmf-local/fonts/opentype/Yu
-        # 游フォントの設定
-        mkdir -p /usr/local/texlive/texmf-local/fonts/opentype/Yu
-        pushd /usr/local/texlive/texmf-local/fonts/opentype/Yu
-        ln -s /System/Library/AssetsV2/com_apple_MobileAsset_Font7/*/AssetData/YuGothic-Medium.otf .
-        ln -s /System/Library/AssetsV2/com_apple_MobileAsset_Font7/*/AssetData/YuGothic-Bold.otf .
-        ln -s /System/Library/AssetsV2/com_apple_MobileAsset_Font7/*/AssetData/YuMincho.ttc .
-        ln -s /System/Library/AssetsV2/com_apple_MobileAsset_Font7/*/AssetData/Kyokasho.ttc .
-        popd
-    end
-    cjk-gs-integrate --cleanup --link-texmf
-else
-    echo 'Skip cjk-gs-integrate'
-end
