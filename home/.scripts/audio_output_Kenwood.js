72cad9448235621c89cb3322cc2f44c773dcc4b1
USAMI Kosuke <usami.kosuke@gmail.com>
2026-01-06 12:10:52 +0900

    Change audio output volume to 75%

--- home/.scripts/audio_output_Kenwood.js
+++ home/.scripts/audio_output_Kenwood.js
@@ -2,6 +2,6 @@ function run(input, parameters) {
     const current = Application.currentApplication()
     current.includeStandardAdditions = true
     current.doShellScript("/opt/homebrew/bin/SwitchAudioSource -t output -s 'KENWOOD KH-M700'")
-    current.setVolume(null, { outputVolume: 90 })
+    current.setVolume(null, { outputVolume: 75 })
     return input;
 }
