04e7e9b190193747ac265a9974a146d26dff6a4c
USAMI Kosuke <usami.kosuke@gmail.com>
2026-03-01 10:05:55 +0900

    wezterm.lua: reorganize configuration settings and enhance initial setup

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -1,12 +1,19 @@
 local wezterm = require "wezterm"
 local config = wezterm.config_builder()
+config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
+config.skip_close_confirmation_for_processes_named = { "fish" }
+config.default_cursor_style = "BlinkingBlock"
 config.color_scheme = "Operator Mono Dark"
 config.font = wezterm.font_with_fallback { "0xProto Nerd Font", "BIZ UDGothic" }
 config.font_size = 14
-config.default_cursor_style = "BlinkingBlock"
-config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
-config.skip_close_confirmation_for_processes_named = { "fish" }
-config.hide_tab_bar_if_only_one_tab = true
+config.initial_cols = 92
+config.initial_rows = 46
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.show_tabs_in_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+wezterm.on("update-right-status", function(window, pane)
+  window:set_right_status(pane:get_title() .. "  ")
+end)
 config.keys = {
   {
     key = "n",
