0b85af5b68898c4d942cdae41435f6a54e6dfa30
USAMI Kosuke <usami.kosuke@gmail.com>
2026-03-24 09:50:02 +0900

    Studioの音量調整

--- home/.scripts/audio_output_Studio.js
+++ home/.scripts/audio_output_Studio.js
@@ -2,6 +2,6 @@ function run(input, parameters) {
     const current = Application.currentApplication()
     current.includeStandardAdditions = true
     current.doShellScript("/opt/homebrew/bin/SwitchAudioSource -t output -u 'Studio Display'")
-    current.setVolume(null, { outputVolume: 50 })
+    current.setVolume(null, { outputVolume: 30 })
     return input;
 }
