4c5e5daca7f2042f1bcf7d0939144a221a5d8c40
USAMI Kosuke <usami.kosuke@gmail.com>
2026-04-30 16:42:05 +0900

    20_brew: explain why brew upgrade --cask follows brew bundle

--- home/.environment/20_brew/eupdate.fish
+++ home/.environment/20_brew/eupdate.fish
@@ -3,6 +3,7 @@
 if type --query brew
     brew bundle --global dump --force
     brew bundle --global
+    # brew bundle は auto_updates true の cask（Chrome/Slack/IDE 系等）の更新をスキップするため補完。
     brew upgrade --cask --greedy-auto-updates
     brew doctor
 else
