1eadcc3bfb4689048049b3c1cbead5ca67b45fcb
USAMI Kosuke <usami.kosuke@gmail.com>
2026-05-04 12:41:51 +0900

    20_brew: brew linkage --test を追加して dylib 参照切れを検知

--- home/.environment/20_brew/eupdate.fish
+++ home/.environment/20_brew/eupdate.fish
@@ -6,6 +6,8 @@ if type --query brew
     # brew bundle は auto_updates true の cask（Chrome/Slack/IDE 系等）の更新をスキップするため補完。
     brew upgrade --cask --greedy-auto-updates
     brew doctor
+    # brew doctor は SOVERSION ミスマッチ等の dylib 参照切れを検知しないため補完。
+    brew linkage --test
 else
     echo 'Skip brew'
 end
