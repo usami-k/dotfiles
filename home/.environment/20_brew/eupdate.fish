fc47947c74b610c4808ee3d94a5b006c33d1ef06
USAMI Kosuke <usami.kosuke@gmail.com>
2026-05-08 08:44:22 +0900

    20_brew: blank lines を追加して可読性を改善

--- home/.environment/20_brew/eupdate.fish
+++ home/.environment/20_brew/eupdate.fish
@@ -2,9 +2,11 @@
 
 if type --query brew
     brew bundle --global dump --force
+
     brew bundle --global
     # brew bundle は auto_updates true の cask（Chrome/Slack/IDE 系等）の更新をスキップするため補完。
     brew upgrade --cask --greedy-auto-updates
+
     brew doctor
     # brew doctor は SOVERSION ミスマッチ等の dylib 参照切れを検知しないため補完。
     brew linkage --test
