4c54c71761aa6676cdf18a3420143c03de22e0c9
USAMI Kosuke <usami.kosuke@gmail.com>
2026-06-13 14:35:49 +0900

    Remove eupdate sudo preflight

--- home/.environment/10_brew/eupdate.fish
+++ home/.environment/10_brew/eupdate.fish
@@ -1,9 +1,6 @@
 #!/usr/bin/env fish
 
 if type --query brew
-    echo "Homebrew, MAS, and cask updates may require sudo for app installers, launchctl services, and package uninstallers."
-    sudo -v
-
     # cask の更新を先に実施しておく。auto_updates true の cask の更新も行う。
     brew upgrade --cask --greedy-auto-updates --no-ask
 
