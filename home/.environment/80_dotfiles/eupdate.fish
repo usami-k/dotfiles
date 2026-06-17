b4f911c92340eda2746259beb93a3f1dd7761e18
USAMI Kosuke <usami.kosuke@gmail.com>
2026-06-17 11:53:53 +0900

    eupdate: pull homeshick castles from each castle root

--- home/.environment/80_dotfiles/eupdate.fish
+++ home/.environment/80_dotfiles/eupdate.fish
@@ -1,11 +1,18 @@
 #!/usr/bin/env fish
 
-if type --query homeshick
-    homeshick --batch pull
-    homeshick --batch --force link
-else
+if not type --query homeshick
     echo 'Skip homeshick'
+    exit 0
 end
+
+for repo_dir in ~/.homesick/repos/*/
+    set -l castle (basename $repo_dir)
+    pushd $repo_dir
+    homeshick --batch pull $castle
+    popd
+end
+
+homeshick --batch --force link
 
 set -l backups ~/.claude.json.backup.*
 if test (count $backups) -gt 0
