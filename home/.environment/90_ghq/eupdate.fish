2b8e69fc9abb7dba0192495c70696bae417e27b2
USAMI Kosuke <usami.kosuke@gmail.com>
2026-07-05 12:20:39 +0900

    Use ghq-repos in ghq eupdate

--- home/.environment/90_ghq/eupdate.fish
+++ home/.environment/90_ghq/eupdate.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
-if type --query ghq
-    ghq list --full-path | while read repo
+if type --query ghq-repos
+    ghq-repos | while read repo
         echo "===" $repo
         pushd $repo
         if test -d .jj
