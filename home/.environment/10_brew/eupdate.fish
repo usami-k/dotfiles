02e63297a8c52ab82ce1055f39f537e4c6602e5b
USAMI Kosuke <usami.kosuke@gmail.com>
2026-07-07 00:37:39 +0900

    Handle Codex Computer Use quarantine

--- home/.environment/10_brew/eupdate.fish
+++ home/.environment/10_brew/eupdate.fish
@@ -4,6 +4,17 @@ if type --query brew
     # cask の更新を先に実施しておく。auto_updates true の cask の更新も行う。
     brew upgrade --cask --greedy-auto-updates --no-ask
 
+    set -l codex_plugins /Applications/Codex.app/Contents/Resources/plugins
+    if test -d $codex_plugins
+        find $codex_plugins -type d -name 'Codex Computer Use.app' -prune 2>/dev/null | while read -l app
+            if not xattr -lr $app 2>/dev/null | string match -q '*com.apple.quarantine*'
+                continue
+            end
+            echo "Remove quarantine attributes from $app"
+            xattr -dr com.apple.quarantine $app
+        end
+    end
+
     brew bundle --global dump --force
 
     brew bundle --global install
